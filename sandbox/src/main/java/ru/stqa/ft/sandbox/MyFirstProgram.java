186ae0ce785ef8c8e820892136d136957edb3d23
nikenyash <124778488+nikenyash@users.noreply.github.com>
2023-02-11 00:07:10 +0400

    Задание 2

--- sandbox/src/main/java/ru/stqa/ft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ft/sandbox/MyFirstProgram.java
@@ -12,6 +12,12 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(3, 8);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+
+		Point p1 = new Point(1.0, 1.0);
+		Point p2 = new Point(3.0, 3.0);
+
+		System.out.println(Point.distance(p1, p2));
+
 	}
 
 	public static void hello(String somebody) {
