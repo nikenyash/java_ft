3e67e4d41f38b6d340fd6523552f86f07c45bf06
nikenyash <124778488+nikenyash@users.noreply.github.com>
2023-02-16 10:10:56 +0400

    Задание 3. Дополнительные кейсы

--- sandbox/src/test/java/ru/stqa/ft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/ft/sandbox/PointTests.java
@@ -6,10 +6,23 @@ import org.testng.annotations.Test;
 public class PointTests {
 
   @Test
-  public void testPoint(){
+  public void testPoint1(){
     Point p1 = new Point(1.0, 1.0);
     Point p2 = new Point(6.0, 1.0);
     Assert.assertEquals(p1.distance(p2), 5);
   }
 
+  @Test
+  public void testPoint2(){
+    Point p1 = new Point(2.0, 4.9);
+    Point p2 = new Point(6.0, 6.1);
+    Assert.assertEquals(p1.distance(p2), 4.176122603564219);
+  }
+
+  @Test
+  public void testPointNegative(){
+    Point p1 = new Point(1.0, 1.0);
+    Point p2 = new Point(6.0, 1.0);
+    Assert.assertEquals(p1.distance(p2), 7.0);
+  }
 }
