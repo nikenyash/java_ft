d5bd3a139751400e8acf4feb7fb08dbc508e7d0e
nikenyash <124778488+nikenyash@users.noreply.github.com>
2023-02-19 11:04:37 +0400

    Задание 3. Доработка тестового сценария, использование метода assertNotEquals

--- sandbox/src/test/java/ru/stqa/ft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/ft/sandbox/PointTests.java
@@ -20,9 +20,9 @@ public class PointTests {
   }
 
   @Test
-  public void testPointNegative(){
-    Point p1 = new Point(1.0, 1.0);
+  public void testPoint3(){
+    Point p1 = new Point(4.0, 2.0);
     Point p2 = new Point(6.0, 1.0);
-    Assert.assertEquals(p1.distance(p2), 7.0);
+    Assert.assertNotEquals(p1.distance(p2), 7.0);
   }
 }
