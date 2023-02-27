24e036f1bc4c05685f0f1011339da13c110633d7
nikenyash <124778488+nikenyash@users.noreply.github.com>
2023-02-27 17:11:46 +0400

    Добавление перехвата исключения в методе isAlertPresent

--- addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/appmanager/NavigationHelper.java
@@ -18,7 +18,7 @@ public class NavigationHelper extends BaseHelper {
   }
 
   public void returnToHomePage() {
-    click(By.linkText("home page"));
+    click(By.linkText("home"));
   }
 
   public void acceptAlert() {
