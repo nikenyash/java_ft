a9b5a93f067745402d4eed1cb04dae9394657a2b
nikenyash <124778488+nikenyash@users.noreply.github.com>
2023-03-01 18:09:30 +0400

    Реализована проверка и обеспечение выполнения предусловий тестов изменения и удаления групп

--- addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.ft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.ft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
