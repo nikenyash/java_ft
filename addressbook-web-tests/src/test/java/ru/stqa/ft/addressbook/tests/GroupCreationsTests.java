48e839e66f1261254498eca8f52413ecdcb0dc7a
nikenyash <124778488+nikenyash@users.noreply.github.com>
2023-03-10 18:00:50 +0400

    Лекция 4.5. Формирование коллекций

--- addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/tests/GroupCreationsTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/tests/GroupCreationsTests.java
@@ -4,15 +4,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationsTests  extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-    int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().gotoGroupPage();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before + 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
   }
 
 }
