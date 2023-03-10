98bc53be17bca8d350a57555c0a60e8afc843702
nikenyash <124778488+nikenyash@users.noreply.github.com>
2023-03-10 18:19:33 +0400

    Лекция 4.6. Добавлена проверка списков в тесте удаления группы

--- addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/model/GroupData.java
@@ -1,5 +1,7 @@
 package ru.stqa.ft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
   private final String name;
   private final String header;
@@ -21,5 +23,20 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
   }
 }
