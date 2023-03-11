b86bbb648442e96c7e082fb41fcb80a89816edef
nikenyash <124778488+nikenyash@users.noreply.github.com>
2023-03-11 13:30:02 +0400

    Лекция 4.8. Поиск максимального элемента в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/model/GroupData.java
@@ -3,24 +3,25 @@ package ru.stqa.ft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
-    this.id = id;
+  public GroupData(int id, String name, String header, String footer) {
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -38,22 +39,8 @@ public class GroupData {
     return footer;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    if (!Objects.equals(id, groupData.id)) return false;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
+  public void setId(int id) {
+    this.id = id;
   }
 
   @Override
@@ -62,6 +49,24 @@ public class GroupData {
             "id=' " + id + '\'' +
             ", name=' " + name + '\'' +
             '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
 
 }
