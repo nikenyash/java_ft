f88b064813088c2f6cacdbfb253c836556a46623
nikenyash <nikita.shirobokov@outlook.com>
2023-03-17 18:07:44 +0400

    Лекция 5.2

--- addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.ft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.ft.addressbook.model.GroupData;
 
@@ -8,12 +9,16 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() throws Exception {
+  @BeforeMethod
+  public void ensurePreconditions() {
     app.getNavigationHelper().gotoGroupPage();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
+  }
+
+  @Test
+  public void testGroupDeletion() throws Exception {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
@@ -24,5 +29,4 @@ public class GroupDeletionTests extends TestBase {
     before.remove(before.size() - 1);
     Assert.assertEquals(before, after);
   }
-
 }
